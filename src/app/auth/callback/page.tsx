33fdd0dbf7bbd823a472288583aebccd2c5e3652
Tomu Maekawa <tomu.m1107@icloud.com>
2025-04-12 14:23:49 +0900

    Fixed forgetting to wrap context provider

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -3,33 +3,36 @@
 import CallBackModal from "@/app/features/auth/callback/componets/CallBackModal";
 import { useSaveLineUserInfo } from "@/app/features/auth/callback/hooks/useSaveLineUserInfo";
 import FirstView from "@/app/features/common/components/FirstView";
+import { RegisterStepsProvider } from "@/app/features/register/context/UseRegisterStepContext";
 import React, { Suspense } from "react";
 
 export const dynamic = "force-dynamic";
 
 const CallbackPageContent = () => {
-	useSaveLineUserInfo();
+  useSaveLineUserInfo();
 
-	return (
-		<main className="bg-green01 w-full h-lvh">
-			<div className="bg-green01 w-full md:w-[400px] mx-auto">
-				<FirstView />
-				<CallBackModal />
-			</div>
-		</main>
-	);
+  return (
+    <main className="bg-green01 w-full h-lvh">
+      <div className="bg-green01 w-full md:w-[400px] mx-auto">
+        <FirstView />
+        <CallBackModal />
+      </div>
+    </main>
+  );
 };
 
 const Page = () => {
-	return (
-		<Suspense
-			fallback={
-				<div className="text-white text-center mt-20">読み込み中...</div>
-			}
-		>
-			<CallbackPageContent />
-		</Suspense>
-	);
+  return (
+    <Suspense
+      fallback={
+        <div className="text-white text-center mt-20">読み込み中...</div>
+      }
+    >
+      <RegisterStepsProvider>
+        <CallbackPageContent />
+      </RegisterStepsProvider>
+    </Suspense>
+  );
 };
 
 export default Page;
