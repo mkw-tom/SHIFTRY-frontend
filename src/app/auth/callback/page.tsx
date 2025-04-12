83f56e9dda5ccd6be0521a24b4077ce9e0ed62b2
Tomu Maekawa <tomu.m1107@icloud.com>
2025-04-12 13:25:38 +0900

    Fixed forgetting to call hook

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,10 +1,12 @@
 import CallBackModal from "@/app/features/auth/callback/componets/CallBackModal";
+import { useSaveLineUserInfo } from "@/app/features/auth/callback/hooks/useSaveLineUserInfo";
 import FirstView from "@/app/features/common/components/FirstView";
 
 import React from "react";
 
 export const dynamic = "force-dynamic";
 const Page = () => {
+	useSaveLineUserInfo(); 
 	return (
 		<main className="bg-green01 w-full  h-lvh">
 			<div className="bg-green01 w-full md:w-[400px] mx-auto">
