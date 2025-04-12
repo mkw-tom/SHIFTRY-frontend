45659ec0b1f63dfdc72e09f6340ef8275d5d2e48
Tomu Maekawa <tomu.m1107@icloud.com>
2025-04-12 13:29:41 +0900

    fix page error

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -1,3 +1,4 @@
+"use clinet";
 import CallBackModal from "@/app/features/auth/callback/componets/CallBackModal";
 import { useSaveLineUserInfo } from "@/app/features/auth/callback/hooks/useSaveLineUserInfo";
 import FirstView from "@/app/features/common/components/FirstView";
