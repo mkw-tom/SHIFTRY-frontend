d72a22d30194cc6727899922d0a41e2b192f986b
Tomu Maekawa <tomu.m1107@icloud.com>
2025-04-20 12:47:06 +0900

    rafactor pageRoute and setup redux-persist

--- src/app/redux/store.ts
+++ src/app/redux/store.ts
@@ -1,16 +1,32 @@
 "use client";
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import store from "./slices/store";
 import user from "./slices/user";
 
-const reduxStore = configureStore({
-	reducer: {
-		user: user,
-		store: store,
-	},
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "../lib/storage";
+import tokenReducer from "./slices/token";
+
+const rootReducer = combineReducers({
+	token: tokenReducer,
+	user: user,
+	store: store,
 });
+
+const persistConfig = {
+	key: "root",
+	storage,
+	whitelist: ["token"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const reduxStore = configureStore({
+	reducer: persistedReducer,
+	middleware: (getDefaultMiddleware) =>
+		getDefaultMiddleware({ serializableCheck: false }),
+});
+export const persistor = persistStore(reduxStore);
 
 export type RootState = ReturnType<typeof reduxStore.getState>;
 export type AppDispatch = typeof reduxStore.dispatch;
-
-export default reduxStore;
